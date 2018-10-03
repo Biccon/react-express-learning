ee619ee4b457bbf3a29796ee0cc28acba9a132a9
biccon <admin@idx.kr>
2018-10-03 23:58:03 +0900

    .

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,12 +1,25 @@
-const express = require('express');
+import express from 'express';
+import { users, add } from '../../database';
 const router = express.Router();
 
 router.get('/:id', (req, res) => {
-    //login;
+    const filtered = users.filter((item) => {
+        console.log(item);
+        console.log(item.id, req.params.id);
+        return item.id === parseInt(req.params.id);
+    })
+    res.send(filtered);
 });
 
-router.post('/:id', (req, res) => {
+router.get('/add/:name', (req, res) => {
     //register
+    const name = req.params.name;
+    add(name);
+    res.send(users);
+})
+
+router.get('/list', (req,res)=>{
+    res.send(users);
 })
 
 module.exports = router;
