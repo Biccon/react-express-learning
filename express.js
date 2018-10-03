7bd709f175fe3dacd1e660bd719665257284dccc
biccon <admin@idx.kr>
2018-10-03 09:57:37 +0900

    .

--- express.js
+++ express.js
@@ -1,7 +1,8 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use('/user', require('./routes/user'));
 app.use('/users', require('./routes/users'));
 
 app.listen(process.env.PORT || port, () => console.log(`Server is running on port ${port}.`));
---
+++ routes/user/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+    //login;
+});
+
+router.post('/:id', (req, res) => {
+    //register
+})
+
+module.exports = router;
